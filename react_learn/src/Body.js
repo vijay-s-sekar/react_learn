8deec97705a5a9c6c595273e4ad9fcbb1358e3b6
vijay s <vijay9047662558@gmail.com>
2024-02-08 23:23:14 +0530

    push 1

---
+++ react_learn/src/Body.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+      <main>Hellow Welocme</main>
+  )
+}
+
+export default Body
---
+++ react_learn/src/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Header = () =>{
+    return(
+        <header>hello</header>
+    )
+    } 
+export default Header;
