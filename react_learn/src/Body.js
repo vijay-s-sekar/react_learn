235013c96e4106215b49f4fa03860fb2c8800ce8
vijay s <vijay9047662558@gmail.com>
2024-02-10 20:11:44 +0530

    use state name change and count increament

--- react_learn/src/Body.js
+++ react_learn/src/Body.js
@@ -1,25 +1,28 @@
 import React from 'react'
-
+import { useState } from "react";
 
 const Body = () => {
+  const [name_change, change_name] = useState(()=>name_selector())
+  const [count,change_count] = useState(1);
+  
+  function countIncreament() {
+    return change_count(aa => aa+1)
+  }
   function name_selector() {
-    let a=['vijay','v1','v2','v3','v4']
-    let select = Math.floor(Math.random()*a.length)
-
-    return a[select]
+    let user_name=['vijay','v1','v2','v3','v4']
+    let select = Math.floor(Math.random() *user_name.length)
+    return user_name[select]
   }
-  const HandleClick = () =>{
-    console.log("succss")
-  }
-  const Double_click =()=>{
-    console.log("double click")
+  
+  function HandleClick() {
+    change_name(name_selector())
+    countIncreament()
   }
   return (
     <main>
-      <p onDoubleClick={()=>{Double_click()}}> 
-        Hellow Welocme {(name_selector())}
-      </p>
-      <button onClick={() => { HandleClick()}}>click</button>
+        Hellow Welocme <b>{name_change}</b>
+        <p>name change count {count}</p>
+      <button onClick={() => { HandleClick()}}>click here to change the name</button>
       </main>
   )
 }
